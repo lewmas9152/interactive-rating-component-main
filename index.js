65897f1e763f015258853d08fc36e9f5cb6216c9
lewmas9152 <smnjuguna22@gmail.com>
2023-07-10 19:08:34 +0300

    made changes to js

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ let main_el= document.getElementsByClassName("main");
 let thankYou_el = document.getElementsByClassName("thankYou");
 let rate_el = document.getElementsByClassName("rate_btn");
 let select_el = document.getElementsByClassName("select");
+let clickedButton;
+for(let i = 0; i < rate_el.length; i++){
+    rate_el[i].addEventListener("click", function(){
+       clickedButton = this; 
+       clickedButton.style.backgroundColor = "#fb7413";
+       nextButton = clickedButton.previousElementSibling;
+        nextButton.style.backgroundColor = "#7c8798";
+        select_el = clickedButton.innerText;
+       
+    })
+  
+}
 
-rate_el[0].addEventListener("click", function(){
-    main_el[0].style.display = "none";
-    thankYou_el[0].style.display = "unset";
-});
+
 
 submitBtn[0].addEventListener("click", function(){
 
     main_el[0].style.display = "none";
     thankYou_el[0].style.display = "unset";
-    select_el[0].innerHTML = rate_el[0].value;
+    
+    
 });
