b8e0bafd32d6f125a27f38205d3966fcf193c8f6
lewmas9152 <smnjuguna22@gmail.com>
2023-07-11 19:10:48 +0300

    made changes in the css

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ let submitBtn = document.getElementsByClassName("submit");
 let main_el= document.getElementsByClassName("main");
 let thankYou_el = document.getElementsByClassName("thankYou");
 let rate_el = document.getElementsByClassName("rate_btn");
-let select_el = document.getElementsByClassName("select");
+let select_el = document.getElementById("select");
 let clickedButton;
+
+function setClickedValue(num){
+    select_el.innerHTML = `You selected ${num} out of 5`;
+}
+
 for(let i = 0; i < rate_el.length; i++){
     rate_el[i].addEventListener("click", function(){
+        for(let j = 0; j < rate_el.length; j++){
+            rate_el[j].style.backgroundColor = "#ffffffb3";
+        }
        clickedButton = this; 
        clickedButton.style.backgroundColor = "#fb7413";
-       nextButton = clickedButton.previousElementSibling;
-        nextButton.style.backgroundColor = "#7c8798";
-        select_el = clickedButton.innerText;
+       nextButton = rate_el[i-1];
+    //    try{
+    //     var prevButton = rate_el[i+1];
+    //     prevButton.style.backgroundColor = "#7c8798";
+    //    }
+    //      catch(err){
+    //         alert("You are at the last button");
+    //      }
+    //     try{
+    //         nextButton.style.backgroundColor = "#7c8798";
+    //     }
+    //     catch(err){
+    //         alert("You are at the first button");
+    //     }
+        
        
     })
   
 }
-
-
 
 submitBtn[0].addEventListener("click", function(){
 
